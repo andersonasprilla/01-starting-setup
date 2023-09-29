eb30bc57babd6a4e52722c4d8fc6898a916c2a2c
Anderson Asprilla <anderson.asprilla@gmail.com>
2023-09-29 08:47:17 -0400

    Child to Parent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
 const App = () => {
   const expenses = [
     {
@@ -21,8 +23,13 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expenses);
+  };
   return (
     <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
